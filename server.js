7609c63d3d705e9b2fd26752644dc86b4cf8a515
christopher demeke <christopher.demeke@gmail.com>
2018-04-21 16:46:14 -0500

    Updated File Structer with Server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var path = require("path");
+var PORT = process.env.PORT || 5000
+
+var app = express();
+
+// Static Files: CSS, Images, Javascript
+app.use(express.static(__dirname+'/public'));
+
+// Routes
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+
+çç
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
